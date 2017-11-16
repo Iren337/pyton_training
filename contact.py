dfa3bfa78c83bf083d9e8faee2839f0240beb209
Irina Slobodchikova <Iren337@mail.ru>
2017-11-16 04:32:40 +0300

    Выделены вспомогательные методы и классы для контакта

---
+++ contact.py
@@ -0,0 +1,25 @@
+class Contact:
+
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_phone, mobile,
+                       work_phone, fax, email, email2, email3, homepage_ru, byear, ayear, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile = mobile
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage_ru = homepage_ru
+        self.byear = byear
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
