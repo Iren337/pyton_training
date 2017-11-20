96c24c7d7a6322f1b130c8a8e5879e977c8893c2
Iren337 <iren337@mail.ru>
2017-11-20 18:15:30 +0300

    Выделен класс для работы с группами и контактами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,13 +14,13 @@ def app(request):
 
 def test_test_add_group(app):
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="11111", header="1111111", footer="111111111"))
+        app.group.create(Group(name="11111", header="1111111", footer="111111111"))
         app.session.logout()
 
 
 def test_test_add_empty_group(app):
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
+        app.group.create(Group(name="", header="", footer=""))
         app.session.logout()
 
 
