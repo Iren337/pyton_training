1c9942f32eabbcc38c896ff1fd5478612b84699d
Irina Slobodchikova <Iren337@mail.ru>
2018-01-15 03:14:02 +0300

    отключаемая проверка для test_del_group

--- fixture/group.py
+++ fixture/group.py
@@ -53,6 +53,16 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cash = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cash = None
+
     def modify_first_group(self):
         self.modify_group_by_index(0)
 
@@ -76,6 +86,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def count(self):
         wd = self.app.wd
